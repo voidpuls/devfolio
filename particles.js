428c7ca42bd707df724b2e563fbb16e274c534f8
Kameron Bennett <catboomfat@gmail.com>
2025-03-04 17:51:23 -0500

    Create particles.js

---
+++ particles.js
@@ -0,0 +1,21 @@
+particlesJS("particles-js", {
+  "particles": {
+    "number": { "value": 156, "density": { "enable": true, "value_area": 800 } },
+    "color": { "value": "#ffffff" },
+    "shape": {
+      "type": "circle",
+      "stroke": { "width": 7, "color": "#000000" },
+      "polygon": { "nb_sides": 5 }
+    },
+    "opacity": { "value": 0.5, "random": false },
+    "size": { "value": 3, "random": true },
+    "line_linked": { "enable": true, "distance": 150, "color": "#ffffff", "opacity": 0.4, "width": 1 },
+    "move": { "enable": true, "speed": 6, "direction": "none" }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": { "onhover": { "enable": true, "mode": "repulse" }, "onclick": { "enable": true, "mode": "push" } },
+    "modes": { "repulse": { "distance": 200, "duration": 0.4 } }
+  },
+  "retina_detect": true
+});
